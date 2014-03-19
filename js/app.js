3c7a7a0d3fc67f75ef3b5d581f8f5c64d8d3d133
Simcha Kanter <simchakanter@gmail.com>
2014-03-19 11:23:56 -0500

    Add basic jquery to show and hide correct elements

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-	$('.ryu').on("mouseenter", function() {
+	$('.ryu').mouseenter(function() {
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
-	});
-	$(".ryu").on("mouseleave", function() {
+	})
+	.mouseleave(function() {
 		$(".ryu-still").show();
 		$(".ryu-ready").hide();
+	})
+	.mousedown(function() {
+		//play hadouken sound
+		$(".ryu-ready").hide();
+		$(".ryu-throwing").show();
+		$(".hadouken").show();
+		// show hadouken and animate to the right side of screen
+	})
+	.mouseup(function() {
+		$(".ryu-throwing").hide();
+		$(".ryu-ready").show();
+		// ryu goes back to steady position
 	})
 });
